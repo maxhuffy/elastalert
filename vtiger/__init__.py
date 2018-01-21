4c208f068ebb7f41d21c29c2cf592d7893a48344
thepluralevan <thepluralevan@users.noreply.github.com>
2018-01-20 21:54:38 -0600

    vtiger

---
+++ vtiger/__init__.py
@@ -0,0 +1,17 @@
+from elastalert.enhancements import BaseEnhancement
+import WSClient
+
+
+class MyEnhancement(BaseEnhancement):
+    # The enhancement is run against every match
+    # ElastAlert will do this for each enhancement linked to a rule
+
+    def process(self, match):
+        if 'client_name' in match:
+            ws = WSClient.Vtiger_WSClient('https://vtiger.iinfosec.net/')
+            ws.doLogin('reports', 'kXtfcZseUTMwzUJW')
+            vt_account = ws.doQuery("select * from Accounts where cf_1072='" +
+                                    match['client_name'] + "';")
+            match['c1'] = vt_account[0]['cf_1046']
+            match['c2'] = vt_account[0]['cf_1048']
+            match['c3'] = vt_account[0]['cf_1070']
