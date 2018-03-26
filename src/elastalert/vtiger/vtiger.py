9b3b3724920ce3dcbfd40c54461f55dd26a83901
Evans Foster <evans.foster@iinfosec.com>
2018-03-26 10:52:01 -0500

    added ege firewall;

--- src/elastalert/vtiger/vtiger.py
+++ src/elastalert/vtiger/vtiger.py
@@ -10,6 +10,7 @@ class clientAlias(BaseEnhancement):
             ws.doLogin('reports', 'kXtfcZseUTMwzUJW')
             vt_account = ws.doQuery("select * from Accounts where cf_1072='" +
                                     match['client_name'] + "';")
+            # concat emails to string and place in match body
             emails = vt_account[0]['cf_1046'] + ', ' + vt_account[0]['cf_1048'] + ', ' + vt_account[0]['cf_1070']
             match['email'] = emails
             pprint.pprint(match)
